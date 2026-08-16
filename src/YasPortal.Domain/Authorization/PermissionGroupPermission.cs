82380c8dc06472bccfd580506e14425c31682ebe
iamstudent2000 <154673399+iamstudent2000@users.noreply.github.com>
2026-08-16 14:25:19 +0330

    Add permissions contained by permission groups

---
+++ src/YasPortal.Domain/Authorization/PermissionGroupPermission.cs
@@ -0,0 +1,18 @@
+namespace YasPortal.Domain.Authorization;
+
+public sealed class PermissionGroupPermission
+{
+    private PermissionGroupPermission() { }
+
+    public PermissionGroupPermission(Guid groupId, Guid permissionId)
+    {
+        GroupId = groupId;
+        PermissionId = permissionId;
+    }
+
+    public Guid GroupId { get; private set; }
+    public Guid PermissionId { get; private set; }
+
+    public PermissionGroup Group { get; private set; } = null!;
+    public Permission Permission { get; private set; } = null!;
+}
