3a975d73b2f6e399f7c8608839c99cb7f2cac38f
iamstudent2000 <154673399+iamstudent2000@users.noreply.github.com>
2026-08-17 14:08:06 +0330

    Preserve target employee when opening access management

---
+++ src/YasPortal.Web/Components/Pages/AccessManagement.razor.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components;
+
+namespace YasPortal.Web.Components.Pages;
+
+public partial class AccessManagement
+{
+    [SupplyParameterFromQuery(Name = "employee")]
+    public Guid? EmployeeQuery { get; set; }
+
+    private Guid? _lastAppliedEmployeeQuery;
+
+    protected override async Task OnParametersSetAsync()
+    {
+        if (EmployeeQuery is not Guid employeeId || employeeId == _lastAppliedEmployeeQuery)
+            return;
+
+        _lastAppliedEmployeeQuery = employeeId;
+        await EmployeeChanged(employeeId);
+    }
+}
